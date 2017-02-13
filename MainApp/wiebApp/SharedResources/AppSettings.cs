21c2bedb9548bc8cd9992cff8fefe09a65a285a5
Raphael Sackstein <Raphael@codeprecise.com>
2017-02-13 15:33:48 +0200

    First Commit.

---
+++ MainApp/wiebApp/SharedResources/AppSettings.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace wiebApp.SharedResources
+{
+    class AppSettings : NotifyPropertyChanged
+    {
+        //MainColor
+        private string _MainColor = "#FA0953";
+        public string MainColor
+        {
+            get { return _MainColor; }
+            set
+            {
+                value = _MainColor;
+                OnPropertyChanged();
+            }
+        }
+
+        //SecondaryColor (mainly dark or light)
+        private string _SecondaryColor = "#fcfdff";
+        public string SecondaryColor
+        {
+            get
+            {
+                return _SecondaryColor;
+            }
+            set
+            {
+                value = _SecondaryColor;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
---
+++ MainApp/wiebApp/SharedResources/NotifyPropertyChanged.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using wiebApp.Annotations;
+
+namespace wiebApp.SharedResources
+{
+    public class NotifyPropertyChanged : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
