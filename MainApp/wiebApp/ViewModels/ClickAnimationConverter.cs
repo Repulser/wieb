6e60c49b453d1aeda66103e1bbaa83bb41eed487
Raphael Sackstein <Raphael@codeprecise.com>
2017-02-14 17:00:33 +0200

    Basic tabcontrol, and a basic animation.

--- MainApp/wiebApp/ViewModels/ClickAnimationConverter.cs
+++ MainApp/wiebApp/ViewModels/ClickAnimationConverter.cs
@@ -1,35 +1,27 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using wieb.SharedResources;
 
 namespace wieb.ViewModels
 {
-    class ClickAnimationConverter : ValueConverter
+    internal class ClickAnimationConverter : ValueConverter
     {
         public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            
-            if (value != null)
+            double result = (double) value;
+            if (result == -1)
             {
-                var result = (double)value;
-                if (result == -1)
-                {
-                    value = AppSettings._MainColor;
-                }
+                value = AppSettings._MainColor;
+            }
 
-                if (result == 0)
-                {
-                    value = AppSettings._DarkMainColor;
-                }
+            if (result == 0)
+            {
+                value = AppSettings._DarkMainColor;
+            }
 
-                if (result == 1)
-                {
-                    value = AppSettings._White;
-                }
+            if (result == 1)
+            {
+                value = AppSettings._White;
             }
             return value;
         }
