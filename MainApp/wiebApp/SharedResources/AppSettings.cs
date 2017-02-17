841e0adea8515f94d4d88b2f60019c1edf5932b0
Raphael Sackstein <Raphael@codeprecise.com>
2017-02-17 12:46:53 +0200

    Commited unfinished changes.

--- MainApp/wiebApp/SharedResources/AppSettings.cs
+++ MainApp/wiebApp/SharedResources/AppSettings.cs
@@ -5,8 +5,18 @@ namespace wieb.SharedResources
 {
     class AppSettings : NotifyPropertyChanged
     {
-        // M A I N   C O L O R S
-        public static string _MainColor = "#f92a46";
+
+        public AppSettings()
+        {
+            _MainColor = _Red;
+            _DarkMainColor = _DarkRed;
+            _SecondaryColor = _White;
+        }
+
+        // /// /// /// /// /// ///
+        // M A I N   C O L O RS //
+        // /// /// /// /// /// ///
+        public static string _MainColor;
         public string MainColor
         {
             get { return _MainColor; }
@@ -17,7 +27,7 @@ namespace wieb.SharedResources
             }
         }
 
-        public static string _DarkMainColor = "#961A2A";
+        public static string _DarkMainColor;
         public string DarkMainColor
         {
             get { return _DarkMainColor; }
@@ -28,8 +38,10 @@ namespace wieb.SharedResources
             }
         }
 
-        //SecondaryColor (mainly dark or light)
-        private string _SecondaryColor = "#fcfdff";
+        // /// /// /// /// /// ///
+        // 2 N D     C O L O RS //
+        // /// /// /// /// /// ///
+        private string _SecondaryColor;
         public string SecondaryColor
         {
             get { return _SecondaryColor; }
@@ -39,8 +51,10 @@ namespace wieb.SharedResources
                 OnPropertyChanged();
             }
         }
-        // B A S I C   C O L O R S
-        //White
+
+        // /// /// /// /// /// ///
+        // W H I T E C O L O RS //
+        // /// /// /// /// /// ///
         public static string _White = "#FFFFFF";
         public string White
         {
@@ -51,8 +65,10 @@ namespace wieb.SharedResources
                 OnPropertyChanged();
             }
         }
-        //Gray
-        public static string _Gray = "#e8e8e8";
+        // /// /// /// /// /// ///
+        // G R E Y   C O L O RS //
+        // /// /// /// /// /// ///
+        private static string _Gray = "#e8e8e8";
         public string Gray
         {
             get { return _Gray; }
@@ -62,8 +78,10 @@ namespace wieb.SharedResources
                 OnPropertyChanged();
             }
         }
-        //Black
-        public static string _Black = "#e8e8e8";
+        // /// /// /// /// /// ///
+        // B L A C K C O L O RS //
+        // /// /// /// /// /// ///
+        private static string _Black = "#e8e8e8";
         public string Black
         {
             get { return _Black; }
@@ -73,9 +91,9 @@ namespace wieb.SharedResources
                 OnPropertyChanged();
             }
         }
-        // C O L O R S   O F   D O O M
-        //B L U E
-        //blue
+        // /// /// /// /// /// ///
+        // B L U E   C O L O RS //
+        // /// /// /// /// /// ///
         private static string _Blue = "#2163ce";
         public string Blue
         {
@@ -86,7 +104,7 @@ namespace wieb.SharedResources
                 OnPropertyChanged();
             }
         }
-        //DarkBlue
+
         private static string _DarkBlue = "#060647";
         public string DarkBlue
         {
@@ -97,8 +115,9 @@ namespace wieb.SharedResources
                 OnPropertyChanged();
             }
         }
-        //R E D
-        //Red
+        // /// /// /// /// /// ///
+        // R E D     C O L O RS //
+        // /// /// /// /// /// ///
         private static string _Red = "#f93636";
         public string Red
         {
@@ -109,7 +128,7 @@ namespace wieb.SharedResources
                 OnPropertyChanged();
             }
         }
-        //DarkRed
+        
         private static string _DarkRed = "#890f0f";
         public string DarkRed
         {
@@ -120,8 +139,10 @@ namespace wieb.SharedResources
                 OnPropertyChanged();
             }
         }
-        //Y E L L O W  &  O R A N G E 
-        //Yellow
+
+        // /// /// /// /// /// /////
+        // Y E L L O W C O L O RS //
+        // /// /// /// /// /// /////
         private static string _Yellow = "#ffdd00";
         public string Yellow
         {
@@ -132,19 +153,21 @@ namespace wieb.SharedResources
                 OnPropertyChanged();
             }
         }
-        //Ornage
-        private static string _Ornage = "#ff9d00";
-        public string Ornage
+
+        private static string _Orange = "#ff9d00";
+        public string Orange
         {
-            get { return _Ornage; }
+            get { return _Orange; }
             set
             {
-                _Ornage = value;
+                _Orange = value;
                 OnPropertyChanged();
             }
         }
-        //G R E E N
-        //Green
+
+        // /// /// /// /// /// /////
+        // G R E E N   C O L O RS //
+        // /// /// /// /// /// /////
         private static string _Green = "#17c617";
         public string Green
         {
@@ -155,7 +178,7 @@ namespace wieb.SharedResources
                 OnPropertyChanged();
             }
         }
-        //DarkGreen
+        
         private static string _DarkGreen = "#007713";
         public string DarkGreen
         {
@@ -166,14 +189,14 @@ namespace wieb.SharedResources
                 OnPropertyChanged();
             }
         }
-        //LightGreen
+        
         private static string _LightGreen = "#1df441";
         public string LightGreen
         {
             get { return _LightGreen; }
             set
             {
-                _Yellow = value;
+                _LightGreen = value;
                 OnPropertyChanged();
             }
         }
