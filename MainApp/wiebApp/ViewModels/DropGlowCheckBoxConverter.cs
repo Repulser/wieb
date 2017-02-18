1df7c4283cedda2d9cf5482df2cd9d16284f531d
Raphael Sackstein <Raphael@codeprecise.com>
2017-02-18 12:45:24 +0200

    Checkbox for dropshadow and dropglow.

---
+++ MainApp/wiebApp/ViewModels/DropGlowCheckBoxConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace wiebApp.ViewModels
+{
+    class DropGlowCheckBoxConverter : ValueConverter
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool valueAsBool = (bool) value;
+            switch (valueAsBool)
+            {
+                case true:
+                    value = Brushes.White;
+                    break;
+                case false:
+                    value = Brushes.Transparent;
+                    break;
+            }
+
+            return value;
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MainApp/wiebApp/ViewModels/DropShadowCheckBoxConverter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using wiebApp.Views;
+
+
+namespace wiebApp.ViewModels
+{
+    class DropShadowCheckBoxConverter : ValueConverter
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool valueAsBool = (bool) value;
+            switch (valueAsBool)
+            {
+                case true:
+                    value = Brushes.Black;
+                    break;
+                case false:
+                    value = Brushes.Transparent;
+                    break;
+            }
+
+            return value;
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            SolidColorBrush valueAsBrush = (SolidColorBrush) value;
+            if (Equals(valueAsBrush, Brushes.Black))
+            {
+                value = true;
+            }
+            else
+            {
+                value = false;
+            }
+            return value;
+        }
+    }
+}
