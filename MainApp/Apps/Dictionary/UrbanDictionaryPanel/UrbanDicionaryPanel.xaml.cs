140f0cc6e80e51c553d5d641e0467630a733ecbc
jeuxjeux20 <jeuxjeux20@outlook.fr>
2017-03-05 00:08:35 +0100

    Updated urbandictionarypanel, tbh there is no data to store in so mvvm is pretty useless at this point.

---
+++ MainApp/Apps/Dictionary/UrbanDictionaryPanel/UrbanDicionaryPanel.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using UrbanDictionnet;
+
+namespace Dictionary.UrbanDictionaryPanel
+{
+    /// <summary>
+    ///     Interaction logic for UrbanDicionaryPanel.xaml
+    /// </summary>
+    public partial class UrbanDictionaryPanel : UserControl
+    {
+        public UrbanDictionaryPanel()
+        {
+            this.InitializeComponent();
+        }
+
+        private UrbanClient Client { get; } = new UrbanClient();
+
+        protected async void OnSearch(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(TextBox.Text))
+            {
+                TextBox.BorderBrush = new SolidColorBrush(Colors.Red);
+                TextBox.BorderThickness = new Thickness(1);
+            }
+            else
+            {
+                TextBox.BorderThickness = new Thickness(0);
+            }
+            try
+            {
+                var definition = await Client.GetWordAsync(TextBox.Text);
+                var sr = new StringBuilder();
+                foreach (var thing in definition)
+                {
+                    sr.AppendLine(thing + "\n -");
+                }
+                TextBox.Text = sr.ToString();
+            }
+            catch (WordNotFoundException)
+            {
+                TextBox.Text = "Word not found";
+            }
+        }
+    }
+}
