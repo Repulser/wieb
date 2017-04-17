80b81db5b3990cd1cc3db428473fb3fae6e35acb
TheMulti0 <Raphael@codeprecise.com>
2017-04-17 13:02:56 +0300

    Deleted junk

--- MainApp/Apps/Dictionary/ViewModels/UrbanItem/UrbanItemViewModel.cs
+++ MainApp/Apps/Dictionary/ViewModels/UrbanItem/UrbanItemViewModel.cs
@@ -1,44 +1,13 @@
-﻿using System;
-using System.Windows;
-using System.Windows.Media;
-using UrbanDictionnet;
+﻿using UrbanDictionnet;
 
 namespace Dictionary.ViewModels
 {
     public class UrbanItemViewModel : NotifyPropertyChanged
     {
-        private SolidColorBrush _thumbsUpFill;
-        private SolidColorBrush _thumbsDownFill;
-
         public UrbanItemViewModel(DefinitionData definitionData)
         {
             Instance = this;
             Definition = definitionData;
-            ThumbsUpFill = new SolidColorBrush();
-            ThumbsDownFill = new SolidColorBrush();
-        }
-
-        public bool ThumbsDownIsChecked { get; set; }
-
-        public bool ThumbsUpIsChecked { get; set; }
-
-        public SolidColorBrush ThumbsUpFill
-        {
-            get => _thumbsUpFill;
-            set
-            {
-                _thumbsUpFill = value;
-                OnPropertyChanged();
-            }
-        }
-
-        public SolidColorBrush ThumbsDownFill
-        {
-            get => _thumbsDownFill;
-            set
-            {
-                _thumbsDownFill = value; OnPropertyChanged();
-            }
         }
 
         public static UrbanItemViewModel Instance { get; set; }
--- MainApp/Apps/Dictionary/Views/UrbanItem/UrbanItem.xaml.cs
+++ MainApp/Apps/Dictionary/Views/UrbanItem/UrbanItem.xaml.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Speech.Synthesis;
+﻿using System.Speech.Synthesis;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Controls.Primitives;
-using System.Windows.Media;
 using Dictionary.Services;
 using Dictionary.ViewModels;
 
@@ -36,33 +33,6 @@ namespace Dictionary.Views
             }
 
             _counter ++;
-        }
-
-        private void Thumbs_OnClick(object sender, RoutedEventArgs e)
-        {
-            //add rectangle forreach togglebutton and bind fill to the property in vm
-            var s = sender as ToggleButton;
-            UrbanItemViewModel vm = UrbanItemViewModel.Instance;
-            if (s?.Name == "ThumbsUp")
-            {
-                if (vm != null)
-                {
-                    var isDisabled = vm.ThumbsUpIsChecked == false;
-                    var color = isDisabled ? Colors.DarkGray
-                        : Colors.Transparent;
-                    vm.ThumbsUpFill.Color = color;
-                }
-            }
-            else
-            {
-                if (vm != null)
-                {
-                        vm.ThumbsUpFill.Color
-                                        = vm.ThumbsUpIsChecked == false
-                                            ? Colors.DarkGray
-                                            : Colors.Transparent;
-                }
-            }
         }
     }
 }
